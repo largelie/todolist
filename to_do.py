a6f5394740ed31a7a763cfb882439e32f176d1bd
largelie <selyzds@gmail.com>
2026-01-29 13:46:09 +0300

    upgraded to do list, add load tasks in json file and time of creating

--- to_do.py
+++ to_do.py
@@ -1,14 +1,92 @@
+# import os
+# import time
+
+# to_do_list = []
+
+# def add():
+#     os.system("cls")
+
+#     added = input("Введите текст задачи для добавления:\n")
+#     to_do_list.append(added)
+#     print("Задача успешно добавлена")
+
+#     time.sleep(3)
+
+#     os.system("cls")
+
+# def sholist():
+#     os.system("cls")
+
+#     if len(to_do_list) > 0:
+#         print("Список задач:")
+#         for i, item in enumerate(to_do_list):
+#             print(f"{i+1}. {item}")
+
+#         time.sleep(3)
+#         os.system("cls")
+#     else:
+#         print("Задач пока нету.")
+
+#         time.sleep(3)
+#         os.system("cls")
+
+# def ready(to_do_list):
+#     os.system("cls")
+#     if not to_do_list:
+#         print("Cписок задач пуст")
+#         time.sleep(3)
+#         os.system("cls")
+#         return
+    
+#     deleted = int(input("Введите номер задачи для пометки как выполненной и ее удаления:\n"))
+    
+
+#     if 1 <= deleted <= len(to_do_list):
+#         deleted -= 1
+#         to_do_list.pop(deleted)
+#         print("Задача удалена.\n")
+
+#         time.sleep(3)
+#         os.system("cls")
+
+# while True:
+#     print("Что вы хотите сделать? Введите\n/ для просмотра всех задач\n+ для добавления задачи\n- для пометки задачи как выполненной и её удаления\n\n")
+#     ans = input()
+#     if ans == "/":
+#         sholist()
+#     elif ans == "+":
+#         add()
+#     elif ans == "-":
+#         ready(to_do_list)
+#     else:
+#         print("Команда не найдена")
+
+
+
+# NEW UPGRADED VERSION
+
+
+
 import os
+from datetime import datetime
+import json
 import time
 
-to_do_list = []
+with open('todo.json', 'r', encoding='utf-8') as f:
+    to_do_list = json.load(f)
 
 def add():
     os.system("cls")
 
     added = input("Введите текст задачи для добавления:\n")
-    to_do_list.append(added)
+
+    current_time = datetime.now().strftime("%H:%M")
+    task_with_time = f"{added} ({current_time})"
+
+    to_do_list.append(task_with_time)
     print("Задача успешно добавлена")
+
+    saving_tasks()
 
     time.sleep(3)
 
@@ -46,12 +124,18 @@ def ready(to_do_list):
         to_do_list.pop(deleted)
         print("Задача удалена.\n")
 
+        saving_tasks()
+
         time.sleep(3)
         os.system("cls")
 
+def saving_tasks():
+    with open('todo.json', 'w', encoding='utf-8') as f:
+        json.dump(to_do_list, f, ensure_ascii=False, indent=2)
+
 while True:
-    print("Что вы хотите сделать? Введите\n/ для просмотра всех задач\n+ для добавления задачи\n- для пометки задачи как выполненной и её удаления\n\n")
-    ans = input()
+    print("Что вы хотите сделать? Введите\n/ для просмотра всех задач\n+ для добавления задачи\n- для пометки задачи как выполненной и её удаления\n")
+    ans = input("Ввод: ")
     if ans == "/":
         sholist()
     elif ans == "+":
@@ -63,6 +147,9 @@ while True:
 
 
 
+
+
+    
 
 
     
